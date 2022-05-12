01ac62f6ebb6ac010c5ecf9d920e991326938e37
Martin Emanuel Maldonado <yo_martuns@hotmail.com>
2022-05-12 14:25:12 -0300

    busqueda_maps

---
+++ src/components/weatherApp.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react';
+import WeatherForm from './weatherForm';
+import WeatherMainInfo from './weatherMain';
+
+export default function WeatherApp() {
+    const [weather, setWeather] = useState(null);
+    useEffect( ()=>{
+        loadInfo('Argentina');
+    },[]);
+
+    useEffect( ()=>{
+        document.title = `Weather | ${weather?.location.name ?? ''}`
+    },[]);
+
+    async function loadInfo( city='london') {
+        try {  
+            // const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`);  // undefined
+            const request = await fetch(`http://api.weatherapi.com/v1/current.json?key=ca8d2053a96e4faba33100829221205&q=${city}&aqi=no`);
+
+            const json = await request.json();
+            setWeather(json);
+        } catch (error) {
+            
+        }
+    }
+    function handleChangeCity(city) {
+       setWeather(null);
+       loadInfo(city);
+    }
+    return (
+    <div className ='small-container'>
+        <h1>Busca el clima por ciudad favorita </h1>
+        <WeatherForm  onChangeCity={handleChangeCity}/>
+        <WeatherMainInfo weather={weather}/>
+    </div>
+    );
+}
+
